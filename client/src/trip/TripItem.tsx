45b48e116f54805d88c8178214f3c2b27a16bd92
andrey100f <andreiforminte19@yahoo.com>
2023-10-14 16:14:18 +0300

    Edited user interface and added sample code for the backend server

--- client/src/trip/TripItem.tsx
+++ client/src/trip/TripItem.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 import {getLogger} from "../utils";
+import {
+    IonButton,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonChip,
+    IonIcon
+} from "@ionic/react";
+import {pencil, trash} from "ionicons/icons";
 
 const log = getLogger('TripItem');
 
@@ -14,15 +25,25 @@ interface TripItemProps {
 const TripItem: React.FC<TripItemProps> = ({id, destination, cost, date, completed}) => {
     log(`render ${destination}, ${cost}, ${date}, ${completed}`);
     return (
-        <div>
-            <div>
-                <p>Destination: {destination}</p>
-                <p>Cost: {cost}</p>
-                <p>Date: {date}</p>
-                <p>Completed: {completed ? "Yes" : "No"}</p>
-            </div>
-            <hr/>
-        </div>
+        <IonCard>
+            <IonCardHeader>
+                <IonCardTitle>{destination}</IonCardTitle>
+                <IonCardSubtitle>{date}</IonCardSubtitle>
+            </IonCardHeader>
+
+            <IonCardContent>
+                <IonChip color={completed ? "success" : "danger"} >{completed ? "Completed" : "Uncompleted"}</IonChip>
+                <br/>
+                <IonButton color="secondary">
+                    <IonIcon slot="start" icon={pencil}></IonIcon>
+                    Update
+                </IonButton>
+                <IonButton color="danger">
+                    <IonIcon slot="start" icon={trash}></IonIcon>
+                    Remove
+                </IonButton>
+            </IonCardContent>
+        </IonCard>
     );
 };
 
